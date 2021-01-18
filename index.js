4a3342dda9251941e38c24ea265830cfa78fd168
lovecosma <williamsmatthew333@gmail.com>
2021-01-18 16:37:46 -0500

    commit tree aa4c0b48cb233955dae72031ebe92bb4b21a57fb

--- index.js
+++ index.js
@@ -3,6 +3,28 @@ function getName(node){
 
 }
 
-function headNode(linked){
-    debugger
+function headNode(linked, collection){
+    return collection[linked]
+}
+
+function next(node, collection){
+    let next = node['next']    
+    return collection[next]
+}
+
+function nodeAt(index, list, collection){
+    let node = headNode(list, collection)
+
+    for(let i = 0; i < index; i++){
+        node = next(node, collection)
+    }
+    return node
+}
+
+function addressAt(index, list, collection){
+    let node = headNode(list, collection)
+    for(let i = 0; i < index; i ++){
+        node = next(node, collection)
+    }
+    return getName(node)
 }
